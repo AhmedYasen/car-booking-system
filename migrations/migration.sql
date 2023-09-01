ef57cd1d83aa418b7930d85684776834fc3bc930
Ahmed Yasen <ahmedyasen8@gmail.com>
2023-09-01 04:36:43 +0300

    Complete Migration queries
    - write queiries of category, vehicle and booking

--- migrations/migration.sql
+++ migrations/migration.sql
@@ -6,3 +6,30 @@ CREATE TABLE IF NOT EXISTS customers (
     PRIMARY KEY (email)
 );
 
+
+CREATE TABLE IF NOT EXISTS category (
+    id INT UNIQUE AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS vehicle (
+    id INT UNIQUE AUTO_INCREMENT,
+    category_id INT NOT NULL,
+    availability BOOLEAN,
+    PRIMARY KEY (id),
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE IF NOT EXISTS booking (
+    id INT UNIQUE AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    vehicle_id INT NOT NULL,
+    hire_date DATETIME NOT NULL,
+    return_date DATETIME NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE,
+    FOREIGN KEY (vehicle_id) REFERENCES vehicle(id) ON DELETE CASCADE
+);
